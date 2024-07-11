0d4f113d451dedc8e8291f70a8bd6be28cf3e7f9
Yuvraj <devyuvraj883@gmail.com>
2024-07-12 01:30:24 +0530

    Added redirect to dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,18 @@
 // import { Dashboard } from "@mui/icons-material";
+"use client"
 import Image from "next/image";
+import { usePathname, useRouter } from "next/navigation";
+
 
 export default function Home() {
+  const pathName = usePathname();
+  const router = useRouter();
+  if(pathName=='/'){
+    router.push('/dashboard');
+  }
   return (
    <div>
-    
+
    </div>
   );
 }
