d7543a90ea2a1cd27950f3f8f18ad0f074911e29
Yuvraj <devyuvraj883@gmail.com>
2024-07-12 04:00:56 +0530

    Updated style

--- app/dashboard/_components/TemplateCard.tsx
+++ app/dashboard/_components/TemplateCard.tsx
@@ -9,7 +9,7 @@ import { TEMPLATE } from './TemplateSection';
 
 const TemplateCard = (template:TEMPLATE) => {
   return (
-    <div className='flex flex-col items-start justified-center p-4 shadow-md h-60 rounded-md '>
+    <div className='flex flex-col items-start justified-center p-4 shadow-md h-60 rounded-md cursor-pointer'>
       <Image src={template.icon} height={80} width={80} alt="icon" />
       <h3 className='text-lg font-semibold'>{template.name}</h3>
       <p className='line-clamp-3 font-normal'>{template.desc}</p>
