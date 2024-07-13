377653cc0decf394a28bf398d8efe0753806b34b
Yuvraj <devyuvraj883@gmail.com>
2024-07-13 22:36:53 +0530

    removing deploy errors

--- app/dashboard/content/[slug]/_components/OutputSection.tsx
+++ app/dashboard/content/[slug]/_components/OutputSection.tsx
@@ -1,12 +1,26 @@
-import React from 'react'
-import '@toast-ui/editor/dist/toastui-editor.css';
+import React, { useRef } from 'react'
+import '@toast-ui/editor/dist/toastui-editor.css'
 
-import { Editor } from '@toast-ui/react-editor';
+import { Editor } from '@toast-ui/react-editor'
+import { Copy } from 'lucide-react';
 
 function OutputSection() {
+  const editorRef = useRef(null);
   return (
-    <div className='col-span-2'>
-      Output section
+    <div className='col-span-2 mt-8 flex flex-col px-2 '>
+      <section className='flex justify-between px-4 py-2 shadow-md items-center bg-white rounded-sm '>
+        <h1 className='font-medium text-lg '>Result</h1>
+        <button className='flex items-center px-3 rounded-md py-1 bg-purple-500 hover:bg-purple-600 text-white font-semibold'><Copy/> Copy </button>
+      </section>
+      <Editor
+        ref= {editorRef}
+        initialValue='The result will appear here!!'
+        previewStyle='vertical'
+        height='540px'
+        initialEditType='wysiwyg'
+        useCommandShortcut={true}
+
+      />
     </div>
   )
 }
