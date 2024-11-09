5aa47de85fa43b6e9bda39c15037b08f348c2a9b
Yuvraj <devyuvraj883@gmail.com>
2024-11-09 07:03:32 +0530

    Updated style

--- app/dashboard/offers/page.tsx
+++ app/dashboard/offers/page.tsx
@@ -3,7 +3,7 @@ import Offers from './_components/Offers'
 
 function page() {
   return (
-    <div className='flex items-center justify-center '>
+    <div className='flex items-center justify-center min-h-screen'>
       <Offers/>
     </div>
   )
