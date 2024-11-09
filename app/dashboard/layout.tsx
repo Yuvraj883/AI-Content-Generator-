ecada40da681645fcdcb98d5cc371bfbe508d16d
Yuvraj <devyuvraj883@gmail.com>
2024-11-09 07:07:11 +0530

    Updated layout

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,6 +1,6 @@
 import Sidebar from "./_components/Sidebar";
-import Header from './_components/Header'
-
+import Header from './_components/Header';
+import Footer from './_components/Footer';
 
 export default function RootLayout({
   children,
@@ -8,19 +8,26 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-
     <html lang="en">
+      <body >
+        {/* Sidebar */}
+        <div className="flex min-h-screen">
+        <Sidebar />
 
-      <body className="fixed">
-        <Sidebar/>
-        <div className="md:ml-[14%]">
-        <Header/>
+        {/* Main content container */}
+        <div className="flex flex-col flex-grow md:ml-[14%]">
+          <Header />
 
-        {children}
+          {/* Main content area that grows to push footer down */}
+          <main className="flex-grow p-4">
+            {children}
+          </main>
+          {/* Footer */}
+</div>
+</div>
+          <Footer />
 
-        </div>
-        </body>
+      </body>
     </html>
-
   );
 }
