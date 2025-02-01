65fcbc5299514a2bf178eb0056651c66b436906e
Yuvraj <devyuvraj883@gmail.com>
2025-02-01 16:13:02 +0530

    Integrated sidebar for mobile UI

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,32 +1,33 @@
+"use client";
 import Sidebar from "./_components/Sidebar";
-import Header from './_components/Header';
-import Footer from './_components/Footer';
+import Header from "./_components/Header";
+import Footer from "./_components/Footer";
+import { useState } from "react";
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: Readonly<{ children: React.ReactNode }>) {
+  const [isSidebarOpen, setSidebarOpen] = useState(false);
+
   return (
     <html lang="en">
-      <body >
-        {/* Sidebar */}
+      <body>
         <div className="flex min-h-screen">
-        <Sidebar />
+          {/* Sidebar */}
+          <Sidebar
+            isOpen={isSidebarOpen}
+            closeSidebar={() => setSidebarOpen(false)}
+          />
 
-        {/* Main content container */}
-        <div className="flex flex-col flex-grow md:ml-[14%]">
-          <Header />
+          {/* Main content container */}
+          <div className="flex flex-col flex-grow md:ml-[14%]">
+            <Header toggleSidebar={() => setSidebarOpen(true)} />
 
-          {/* Main content area that grows to push footer down */}
-          <main className="flex-grow p-4">
-            {children}
-          </main>
-          {/* Footer */}
-</div>
-</div>
-          <Footer />
-
+            {/* Main content */}
+            <main className="flex-grow p-4">{children}</main>
+            <Footer />
+          </div>
+        </div>
       </body>
     </html>
   );
